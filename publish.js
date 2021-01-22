7162e6430717a3b881a30459f845b574168f599a
erriy <erriy@msn.com>
2021-01-22 18:45:00 +0800

    update

--- publish.js
+++ publish.js
@@ -1,12 +1,10 @@
 const { exec } = require('child_process');
 
 
-exec(`git tag v${process.env.npm_package_version} && git push && git push --tags`, (err, stdout, stderr) => {
+exec(`git tag v${process.env.npm_package_version}`, (err, stdout, stderr) => {
     if(err) {
-        console.log(err);
         return;
     }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+    exec('git push && git push --tags');
 });
 
