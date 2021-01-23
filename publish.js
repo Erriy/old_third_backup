6ad7a3b5a5b68d9e95c4ac04dc5641eb3641bf25
erriy <erriy@msn.com>
2021-01-23 14:11:16 +0800

    update

--- publish.js
+++ publish.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-
+// todo: 使用最后一次提交的package.json中的version进行判断
 exec(`git tag v${process.env.npm_package_version}`, (err, stdout, stderr) => {
     if(err) {
         return;
