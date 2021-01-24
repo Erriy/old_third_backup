c81d8f860618ed05ed87e38372963d71db45b07a
erriy <erriy@msn.com>
2021-01-24 13:27:05 +0800

    update

--- app/modules/clipboard.js
+++ app/modules/clipboard.js
@@ -3,9 +3,9 @@ const key_sender = require('node-key-sender');
 
 
 function paste(data) {
-    clipboard.writeText(data);
     setTimeout(async ()=>{
         // todo 增加对mac系统的判断
+        clipboard.writeText(data);
         await key_sender.sendCombination(['control', 'v']);
     }, 100);
 }
