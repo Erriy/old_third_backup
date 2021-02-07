0a8d02f23e526c445f1addfb2bb63fcc0bc3b2f9
erriy <erriy@msn.com>
2021-02-07 15:09:56 +0800

    update

--- app/old_webdav/index.js
+++ app/old_webdav/index.js
@@ -1,12 +1,25 @@
 const webdav = require('webdav-server').v2;
-const neofs = require('./neofs');
+// const neofs = require('./neofs');
+const open = require('open');
+const cmd_exists = require('command-exists');
 
 const server = new webdav.WebDAVServer({
     // rootFileSystem: new neofs.filesystem()
     rootFileSystem: new webdav.PhysicalFileSystem('/home/erriy/Downloads/')
 });
 
-server.start(() => server.stop());
+server.start(async () => {
+    let app = undefined;
+    for(let a of ['nautilus', 'thunar']) {
+        try {
+            await cmd_exists(a);
+            app = a;
+            break;
+        }
+        catch(e) {}
+    }
+    await open('dav://localhost:1900/', {wait: false, app:app});
+});
 
 console.log('test');
 
--- app/webdav/index.js
+++ app/webdav/index.js
@@ -1,7 +1,14 @@
 const webdav = require('webdav-server').v2;
+const os = require('os');
+const open = require('open');
+const cmd_exists = require('command-exists');
 
 let obj = {
     server: null,
+    options: {
+        host: undefined,
+        port: undefined,
+    },
 };
 
 function stop() {
@@ -10,6 +17,42 @@ function stop() {
         obj.server = null;
     }
 }
+
+const open_handler = {
+    linux: async ()=>{
+        let app = undefined;
+        for(let a of ['thunar', 'nautilus']) {
+            try {
+                await cmd_exists(a);
+                app = a;
+                break;
+            }
+            catch(e) {}
+        }
+        await open('dav://localhost:1900/', {wait: false, app:app});
+    },
+    win32: async()=>{
+        // todo win32 open handler
+    },
+    mac: async()=>{
+        // todo mac open handler
+    },
+};
+
+const mount_handler = {
+    linux: async ()=>{
+        return open_handler.linux;
+    },
+    win32: async()=>{
+        // todo win32 mount handler
+    },
+    mac: async()=>{
+        // todo mac mount handler
+    },
+};
+
+const wabdav_open = open_handler[os.platform()];
+const mount = mount_handler[os.platform()];
 
 function start({
     host='localhost',
@@ -22,9 +65,11 @@ function start({
         // rootFileSystem: new neofs.filesystem(),
     });
     obj.server.start();
+    mount();
 }
 
 module.exports = {
     start,
     stop,
+    open: wabdav_open,
 };
