e57d9909fcff940c16f1989c76d11143b22849cd
erriy <erriy@msn.com>
2021-03-25 13:59:58 +0800

    update

--- app/service/seed.js
+++ app/service/seed.js
@@ -63,6 +63,29 @@ router.put('', async(req, res)=>{
     // todo 创建连接
 });
 
+router.put('/:seedid/tag/:tag', async (req, res)=>{
+    // todo 标签去重
+    let result = await res.neo.run(
+        `
+            match (s:seed{id:$seedid})
+            call {
+                with s with s where exists(s.tag) return s.tag as old_tags
+                union
+                with s with s where not exists(s.tag) return [] as old_tags
+            }
+            set s.tag = old_tags + $tag
+            return s.tag as tag
+        `,
+        {seedid: req.params.seedid, tag: req.params.tag}
+    );
+    let data = result.records.length > 0 ? result.records[0].get('tag') : [];
+    return res.build({data});
+});
+
+router.delete('/:seedid/tag/:tag', async (req, res)=>{
+    // todo 删除标签
+});
+
 module.exports = {
     async router(neo4j_session) {
         try{
