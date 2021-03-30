bbf5a7b6f28e3dd70ce4736491d4da2cf7a847bd
erriy <erriy@msn.com>
2021-03-30 14:48:17 +0800

    删除代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 const Vue = require('vue').default;
 const VueRouter = require('vue-router').default;
-const list = require('../views/list.vue').default;
-const test = require('@/views/test.vue').default;
 const fm = require('../views/fm.vue').default;
 
 Vue.use(VueRouter);
@@ -11,22 +9,6 @@ const routes = [
         path: '/',
         component: fm
     },
-    {
-        path: '/list',
-        component: list
-    },
-    {
-        path: '/seed',
-        component() {
-            return require('../views/seed.vue').default;
-        }
-    },
-    {
-        path: '/service_config',
-        component(){
-            return require('../views/service_config.vue').default;
-        }
-    }
 ];
 
 const router = new VueRouter({
