da2e006f806e6576180c5f8f8e8ed9e3d04acc1b
erriy <erriy@msn.com>
2021-03-30 15:16:47 +0800

    update

---
+++ app/modules/runtime.js
@@ -0,0 +1,36 @@
+const gpg = require('../common/gpg');
+const electron_store = require('electron-store');
+const store = new electron_store();
+
+async function _get_config(key, default_value_func) {
+    let c = store.get(key);
+    if (!c) {
+        c = default_value_func();
+        if(c instanceof Promise) {
+            c = await c;
+        }
+        store.set(key, c);
+    }
+    return c;
+}
+
+async function fingerprint() {
+    return _get_config('fingerprint', async ()=>{
+        let keys = await gpg.list_secret_key();
+        if(keys.length === 0) {
+            throw new Error('请使用gpg创建证书后再使用');
+        }
+        return keys[0].fingerprint;
+    });
+}
+
+function service() {
+    return _get_config('service', ()=>{
+        return 'http://localhost:6952/api';
+    });
+}
+
+module.exports = {
+    fingerprint,
+    service,
+};
